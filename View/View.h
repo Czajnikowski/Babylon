6bb7b9de9a4fcfced2634de8f075e31345949339
Maciek Czarnik <czajnikowski@gmail.com>
2019-06-25 10:23:06 +0200

    Initial Commit

---
+++ View/View.h
@@ -0,0 +1,19 @@
+//
+//  View.h
+//  View
+//
+//  Created by Maciek on 25/06/2019.
+//  Copyright © 2019 mczarnik.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for View.
+FOUNDATION_EXPORT double ViewVersionNumber;
+
+//! Project version string for View.
+FOUNDATION_EXPORT const unsigned char ViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <View/PublicHeader.h>
+
+
